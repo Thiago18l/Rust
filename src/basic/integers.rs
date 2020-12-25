5371cb7d3410e30a610acba5a6b462670d6e4546
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-12-25 16:14:01 -0300

    feat: arithmetics with float and integers

--- src/basic/integers.rs
+++ src/basic/integers.rs
@@ -1,4 +1,16 @@
 fn main() {
-    print!("My number: {}", 140);
+    println!("My number: {}", 140);
+    arithmetics();
+    float_();
+}
+fn arithmetics() {
+    println!("The sum is: {}", 80 + 34);
+    println!("{} + {} = {}", 34, 80, 80 + 34);
+    println!("{}", (23 - 6) % 5 + 20 * 30 / (3 + 4)) // output is 87
+}
+
+fn float_() {
+    println!("The sum is: {}", 80.3 + 34.8);
+    println!("{} {}", -12 % 10, -1.2 % 1.);
 }
 

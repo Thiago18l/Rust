b5d9267682d946193f16698353cb8a143bc16c50
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-12-25 16:24:16 -0300

    chore: add sequence statements in rust

---
+++ src/basic/sequence_statements.rs
@@ -0,0 +1,12 @@
+
+fn main() {
+    print!("{} + ", 80);print!("{} = ", 34);
+        print ! ( "{}" , 
+    80 + 34) ;
+}
+
+// Rust ignore blanks, tabs and line breaks, However, Rust programmers have the following
+// habits that are recommended:
+// 1 - to indent line by four spaces inside functions
+// 2 - to avoid adding several consecutive spaces inside statements
+// 3 - to avoid exceeding 80 columns, possibly splitting long statements on several lines

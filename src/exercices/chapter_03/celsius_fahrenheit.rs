492bbba534e189a4a73f991442758b5a7e736414
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-12-27 16:02:48 -0300

    chore: celsius to fahrenheit exercice

---
+++ src/exercices/chapter_03/celsius_fahrenheit.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Please pass in a celsius or farenheit value to convert.\n\n Examples: -c 50\n -f 10");
+        panic!("Invalid command line argument");
+    }
+
+    let flag = &args[1];
+    let value: f64 = args[2].parse().expect("Invalid number");
+    match flag.as_ref() {
+        "-f" => println!("{}", (value - 32 as f64) / 1.8),
+        "-c" => println!("{}", (value * 1.8) + 32 as f64),
+        _ => println!("No such flag \"{}\"\n\nValid flags are -c -f", flag),
+    }
+}

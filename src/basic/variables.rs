13cd558d8e4981d832910c2a8d09ec6cfa576615
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-12-27 00:30:56 -0300

    chore: shadow variables concept in rustlang

--- src/basic/variables.rs
+++ src/basic/variables.rs
@@ -20,9 +20,24 @@ fn values() {
     x = 20;
     println!("{}", x);
     constants();
+    shadow();
+    strings();
 }
 
 fn constants() {
     const MAX_POINTS: u32 = 100_000;    
     println!("{}", MAX_POINTS);
+}
+
+fn shadow() {
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("the value of x: {}", x);
+}
+
+fn strings() {
+    let spaces = "   ";
+    let spaces = spaces.len(); // the use of let is important to create a other variable with same name as the before
+    println!("{}", spaces);
 }

c62087cf93a0a97f9693ab6f838817eb632a5117
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-12-27 00:16:48 -0300

    chore: add function values

--- src/basic/variables.rs
+++ src/basic/variables.rs
@@ -11,4 +11,12 @@ fn mutable() {
     print!("before: {}\n", number);
     number = 20;
     print!("after: {}\n", number);
+    values();
+}
+
+fn values() {
+    let mut x = 5; // the keyword mut make the variable mutable to change the content inside it
+    println!("{}", x);
+    x = 20;
+    println!("{}", x);
 }
